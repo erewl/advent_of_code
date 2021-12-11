cc4c4aef198776b6c06e5d9d0f23f517a90b33e2
erewl <grunert.katrin@protonmail.com>
2021-12-11 23:08:35 +0100

    Structure code with functions

--- days/day_one.py
+++ days/day_one.py
@@ -1,13 +1,17 @@
 
-def day_one(inputFile):
-    f = open(inputFile, "r")
-    rows = f.read().split("\n")[:-1]
-    res = [row for idx, row in enumerate(rows[1:], start=1) if int(row) > int(rows[idx-1])]
-    return len(res)
+def day_one(input_file):
+    rows = __get_rows(input_file)
+    return len(__filter_for_increasing_rows(rows))
+
+def __get_rows(file):
+    f = open(file, "r")
+    rows_as_int = [int(row) for row in f.read().split("\n")[:-1]]
+    return rows_as_int
+
+def __filter_for_increasing_rows(rows):
+    return [row for idx, row in enumerate(rows[1:], start=1) if row > rows[idx-1]]
 
 def day_one_2(input_file):
-    f = open(input_file, "r")
-    rows = [int(s) for s  in f.read().split("\n")[:-1]]
+    rows = __get_rows(input_file)
     sums = [sum(rows[i:i+3]) for i in range(0, len(rows)-2)]
-    res = [row for idx, row in enumerate(sums[1:], start=1) if int(row) > int(sums[idx-1])]
-    return len(res)
+    return len(__filter_for_increasing_rows(sums))

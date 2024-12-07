d4cf8ac74edb40e72c0f0063276517cccf72be76
erewl <grunert.katrin@protonmail.com>
2024-12-07 20:16:55 +0100

    Executing day 3

--- AoC_2024/src/main.rs
+++ AoC_2024/src/main.rs
@@ -1,10 +1,10 @@
-#[path = "days/day_02.rs"] mod day;
+#[path = "days/day_03.rs"] mod day;
 
 fn main() {
 
-    let day: &str = "02";
     println!("Test Part 1: {:?}", day::pt_01(&format!("./test/{day}.txt")));
     println!("Part 1: {:?}", day::pt_01(&format!("./input/{day}.txt")));
+    let day: &str = "03";
     println!("Test Part 2: {:?}", day::pt_02(&format!("./test/{day}.txt")));
     println!("Part 2: {:?}", day::pt_02(&format!("./input/{day}.txt")));
 }
